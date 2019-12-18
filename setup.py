40bdf7fd6a9fa4855a264831abfb4677b5d2a2c3
cn <cn@mycrobase.de>
2019-12-18 19:44:10 +0100

    module: convert to use Pipfile

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup
 
 setup(name='veml6070',
-      version='1.0',
-      url='http://github.com/cmur2/python-veml6070',
+      version='1.0.0',
+      packages=['veml6070'],
+
+      install_requires=['smbus2'],
+      python_requires='>=2.7',
+
+      url='https://dev.mycrobase.de/gitea/cn/python-veml6070',
       author='Christian Nicolai',
       description=' A python library for accessing the VEML6070 digital UV light sensor from Vishay.',
-      packages=['veml6070'],
-      long_description=open('README.md').read(),
-      requires=['python (>= 2.7)', 'smbus (>= 0.4.1)'],
-      install_requires=['smbus-cffi'])
+      long_description=open('README.md').read())
